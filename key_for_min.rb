1c101822db5d7a8a7e43c684cf9cc02c1d8ee9ef
Joseph Senders <jasenders@gmail.com>
2018-08-12 18:46:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,5 +9,13 @@ else
      name_hash.each do |key, value|
        array << value
 end
+min_value = array[0]
+    array.each do |value|
+      min_value = value if value < min_value
+    end
+    name_hash.each do |key, value|
+      if min_value == value
+        return key
+      end
 end
 end
